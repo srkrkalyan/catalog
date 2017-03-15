e407f41f02466c6a25994b4b326787a6a80c11cf
srkrkalyan <srkrkalyan@gmail.com>
2017-03-14 22:12:33 -0600

    format lotsofmenu for pep8

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 from database_setup import Restaurant, Base, MenuItem, User
 
-engine = create_engine('sqlite:///restaurantmenuwithusers.db', echo = True)
+engine = create_engine('sqlite:///restaurantmenuwithusers.db', echo=True)
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
@@ -21,7 +21,7 @@ session = DBSession()
 
 # Create dummy user
 User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
-           picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
+             picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
 
 #User1 = User(name="Robo Barista")
 session.add(User1)
